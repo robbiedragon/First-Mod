79256ced05beda0a8166cec4ca77864bd1e0677e
robbiedragon <alexsignups9999@bellsouth.net>
2015-01-31 13:26:51 -0500

    Stuff
    YEs

--- src/main/java/com/hayes/ourmod/MainClass.java
+++ src/main/java/com/hayes/ourmod/MainClass.java
@@ -1,5 +1,13 @@
 package com.hayes.ourmod;
 
+import com.example.examplemod.ExampleMod;
+
+import cpw.mods.fml.common.Mod;
+
+
+@Mod(modid = MainClass.MODID, version = MainClass.VERSION)
 public class MainClass {
+	  public static final String MODID = "HayesMod";
+	    public static final String VERSION = "0.1.0 BETA";
 
 }
